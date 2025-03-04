65ba982bb0a6f6b4100fd1d86fd5ac382160b613
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-03-04 06:56:15 -0700

    set up inital project and build basic Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return(
+        <section className="homepage-container">
+            <header className="homepage-header">Aliyah's Art!</header>
+        </section>
+    )
+}
+
+export default Home;
